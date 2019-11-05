d8b50953c6d723405e9c10e61dde324d4388897f
Arthaskj <1285707427@qq.com>
2019-11-05 18:06:01 +0800

    新增浏览量显示

--- docs/api/modules/common.js
+++ docs/api/modules/common.js
@@ -1,5 +1,5 @@
-import request from '../request';
-import requestParam from '../requestParam';
+import request from '../request.js';
+import requestParam from '../requestParam.js';
 
 // 登录
 export function GetContent(params) {
--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,7 @@
  * @Date: 2019/09/11 18:49:44
  * @Description: 系统配置项目
  */
-module.exports = {
+export default {
   httpPort: 8000,
-  ajaxUrl: 'http://arthaskj.cn:3000/'
+  ajaxUrl: 'http://arthaskj.cn:3000'
 }
