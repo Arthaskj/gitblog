caa0a64c7e61f7aeb95d22ce66491c2bd87f3dc0
Arthaskj <1285707427@qq.com>
2019-12-31 16:37:06 +0800

    [Feature]ä½备案信息补充

--- docs/README.md
+++ docs/README.md
@@ -1,23 +1,14 @@
 **遥远的故乡，对生活的企望，生而不断的孤独，压抑，成了这个城市冬日化不开的一团雾，这团雾从过往，从故地飘来，坐落在看不见未来与尽头的黄棕色土地间。**
 
-<video id="video" width="100%" controls="" preload="none">
+<!--<video id="video" width="100%" controls="" preload="none">
       <source id="mp4" src="./src/travel/assets/cs20191105.mp4" type="video/mp4">
      
       <p>Your user agent does not support the HTML5 Video element.</p>
-</video>
+</video>-->
  <!-- <source id="webm" src="http://media.w3.org/2010/05/sintel/trailer.webm" type="video/webm">
       <source id="ogv" src="http://media.w3.org/2010/05/sintel/trailer.ogv" type="video/ogg"> -->
 <br>
 <br>
-
-> 微信二维码
-
-![微信二维码](assets/TIM图片20191017123106.jpg)
-
-> 微信公众号
-
-![微信公众号](assets/默认标题_横版二维码_2019.10.17.png)
-
 <!-- <div id="main">
   <div>hello {{ msg }}</div>
 
--- docs/index.js
+++ docs/index.js
@@ -1,38 +1,38 @@
-import config from './config.js'
-
-const ajax = (option) => {
-  var request = new XMLHttpRequest();
-  let url = config.ajaxUrl + option.url
-  request.open(option.type || 'GET', url + option.url, option.async === undefined ? true : false)
-  if (option.type === 'POST') {
-    // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  }
-  request.onload = function() {if (this.status >= 200 && this.status < 400) {option.success(JSON.parse(this.response))}}
-  request.onerror = function() {option.error(JSON.parse(this.response))}
-  request.send(option.data || null)
-}
-
+// import config from './config.js'
+//
+// const ajax = (option) => {
+//   var request = new XMLHttpRequest();
+//   let url = config.ajaxUrl + option.url
+//   request.open(option.type || 'GET', url + option.url, option.async === undefined ? true : false)
+//   if (option.type === 'POST') {
+//     // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//   }
+//   request.onload = function() {if (this.status >= 200 && this.status < 400) {option.success(JSON.parse(this.response))}}
+//   request.onerror = function() {option.error(JSON.parse(this.response))}
+//   request.send(option.data || null)
+// }
+//
+// // ajax({
+// //   type: 'GET',
+// //   url: "/api/AboutContronller/GetContent",
+// //   success(res) {
+// //     console.log(res);
+// //   }
+// // })
+// let s = window.location.host + ',' + new Date().toLocaleString().replace(/\s/g, '').replace(/\//g, '-');
 // ajax({
 //   type: 'GET',
-//   url: "/api/AboutContronller/GetContent",
+//   url: "/api/SysContronller/SetPageViews?url=" + s,
+//   async: false,
 //   success(res) {
-//     console.log(res);
+//     ajax({
+//       type: 'GET',
+//       url: "/api/SysContronller/GetPageViews",
+//       async: false,
+//       success(res) {
+//         window.pageViews = res && res.length || '0'
+//         console.log(res);
+//       }
+//     })
 //   }
 // })
-let s = window.location.host + ',' + new Date().toLocaleString().replace(/\s/g, '').replace(/\//g, '-');
-ajax({
-  type: 'GET',
-  url: "/api/SysContronller/SetPageViews?url=" + s,
-  async: false,
-  success(res) {
-    ajax({
-      type: 'GET',
-      url: "/api/SysContronller/GetPageViews",
-      async: false,
-      success(res) {
-        window.pageViews = res && res.length || '0'
-        console.log(res);
-      }
-    })
-  }
-})
